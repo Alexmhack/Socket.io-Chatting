f25b7d934146fd348880faab86a42d9871a33fe4
alexHooper <alexmohack@gmail.com>
2018-11-22 19:47:16 +0530

    server index.html file over node server

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', (req, res) => {
-	res.send('<h1>Hello World!!</h1>');
+	// res.send('<h1>Hello World!!</h1>');
+	
+	res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, () => {
