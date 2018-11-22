a67b4d1c582e72e56a2769e81343da9d33c2ab67
alexHooper <alexmohack@gmail.com>
2018-11-22 20:13:40 +0530

    send and receive msg

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
 	// res.send('<h1>Hello World!!</h1>');
 	
 	res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connect', (socket) => {
+	console.log('a user connected...');
+
+	socket.on('chat message', (msg) => {
+		console.log('message arrived: ' + msg);
+		io.emit('chat message', msg);
+	})
+
+	socket.on('disconnect', () => {
+		console.log('a user disconnected...');
+	})
+})
 
 http.listen(3000, () => {
 	console.log('listening on *:3000');
